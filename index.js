840f94284476848c2942ea97e56a04fcacd4bb2c
TowsifMuhtadiKhan <towsifanroidmarket@gmail.com>
2023-11-23 18:29:53 +0600

    reducer added

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-//defining constant
+//Prcess of Redux
+//-----------------------------
+
+//1. State
+//2. dispatch action
+//3. reducer-->based on action type
+//4.update
+
+//-----------------------------
+
+
+//defining constant its a good practice and will help to avoide typos
 const INCREMENT = 'INCREMENT';
 const DECREMENT = 'DECREMENT';
-const ADD_USER = 'ADD_USER';
+// const ADD_USER = 'ADD_USER';
 
 //state
 const initialCounterState={
@@ -17,26 +28,49 @@ const incrementCounter=()=>{
     return {
         type: INCREMENT,
     }
-
 };
 
 const deccrementCounter=()=>{
     return {
         type: DECREMENT,
     }
-
 };
 
-const addUser=()=>{
-    return {
-        type: ADD_USER,
-        payload:{name: 'towsif'},
+// create reducer for counter 
+// reducer is a pure function because it will definitely take input and return output
+//reducer will handle the logic and update state based on action's type
+
+const counterReducer=(state=initialCounterState, action)=>{
+    switch (action.type) {
+        case INCREMENT:
+            return {
+                ...state,//means rest of the property will be here if it has and only change the count
+                count: state.count +1,
+            }
+
+        case DECREMENT:
+            return {
+                ...state,//means rest of the property will be here if it has and only change the count
+                count: state.count -1,
+            }
+        default:
+            state;
     }
+}
 
-};
-//1. State
-//2. dispatch action
-//3. reducer-->based on action type
-//4.update
+
+
+
+
+
+
+
+// const addUser=()=>{
+//     return {
+//         type: ADD_USER,
+//         payload:{name: 'towsif'},
+//     }
+// };
+
 
 
